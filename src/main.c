6cad551b3ab5066f978b48f66a8543a1af4f3e71
Max B. <maksym_borovyk@ukr.net>
2025-03-28 15:43:40 +0200

    Add check for scanf

--- src/main.c
+++ src/main.c
@@ -4,15 +4,20 @@
 int main(){
     int min_bound = 0;
     int max_bound = 9;
-    int rand = get_random_number(min_bound, max_bound);
+    int random_number = get_random_number(min_bound, max_bound);
 
     int user_guess;
     printf("Write your guess\n");
-    scanf("%d", &user_guess);
+    int elements_written = scanf("%d", &user_guess);
+    if (elements_written == 0){
+        fprintf(stderr, "Error: elements_written %d\n", elements_written);
+        return 1;
+    }
+    
     printf("User guess: %d\n", user_guess);
 
     printf("Guess is ");
-    if (rand == user_guess){
+    if (random_number == user_guess){
         printf("correct\n");
     } else {
         printf("not correct\n");
