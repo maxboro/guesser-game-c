a804ea9fc423db4f135abecde0887839e6064fef
Max B. <maksym_borovyk@ukr.net>
2025-03-28 21:18:52 +0200

    Interrupt session on invalid input

--- src/main.c
+++ src/main.c
@@ -7,15 +7,16 @@ int main(){
     int max_bound = 9;
     int n_guesses_allowed = 3;
     bool is_win = false;
+    bool is_error = false;
 
     int random_number = get_random_number(min_bound, max_bound);
 
     for (int n_guess = 0; n_guess < n_guesses_allowed; n_guess++){
-        game_cycle(random_number, &is_win);
-        if (is_win)
+        game_cycle(random_number, &is_win, &is_error);
+        if (is_win || is_error)
             break;
     }
 
-    final_message(is_win);
+    final_message(is_win, is_error);
     return 0;
 }
--- src/user_interactions.c
+++ src/user_interactions.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int get_guess(){
+int get_guess(bool* is_error_ptr){
     int user_guess;
     printf("Write your guess\n");
     int elements_written = scanf("%d", &user_guess);
     if (elements_written == 0){
         fprintf(stderr, "Error: elements_written %d\n", elements_written);
+        *is_error_ptr = true;
         return -1;
     }
     printf("User guess: %d\n", user_guess);
     return user_guess;
 }
 
-void final_message(bool is_win){
-    printf("Game over: You ");
-    if (is_win){
-        printf("won\n");
+void final_message(bool is_win, bool is_error){
+    if (is_error) {
+        printf("Error occured, game is interrupted\n");
     } else {
-        printf("lost\n");
+        printf("Game over: You ");
+        if (is_win){
+            printf("won\n");
+        } else {
+            printf("lost\n");
+        }
     }
 }
 
-void game_cycle(int random_number, bool* is_win_ptr){
-    int user_guess = get_guess();
+void game_cycle(int random_number, bool* is_win_ptr, bool* is_error_ptr){
+    int user_guess = get_guess(is_error_ptr);
+    if (*is_error_ptr){
+        return;
+    }
 
     printf("Guess is ");
     if (random_number == user_guess){
